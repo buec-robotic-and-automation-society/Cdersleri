90aa2de834c13969880572825fb46b2049918fe3
Oguzhan-P <53702544+Oguzhan-P@users.noreply.github.com>
2019-10-02 13:02:50 +0300

    Add files via upload

---
+++ derstekilere örnek kodlar/dortislem.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int a=3, b=4;
+	printf("%d %d %d %d\n", a+b, a-b , a*b, b/a);
+	float c=5, d=2;
+	printf("%f\n",c/d);
+	return 0;
+}
---
+++ derstekilere örnek kodlar/tanimlar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	int a=3;
+	char b='A';
+	float c=2.3;
+	double d=3.8;
+	printf("%d %c %f %lf",a,b,c,d);
+	return 0;
+}
